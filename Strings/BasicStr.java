410652fa3868e1d5928c39464dee5a5ddf27b472
Shrey K <srkkadia@gmail.com>
2023-09-08 20:12:21 +0530

    String Programs

--- Strings/BasicStr.java
+++ Strings/BasicStr.java
@@ -63,4 +63,61 @@ String Tasks:
 
 5. String Case Conversion: Convert 's4' to uppercase and 's2' to lowercase. Print both modified strings. Ensure that the original strings remain unchanged.
 
+6. Word Count: Create a program that takes a sentence as input and counts the number of words in it without using any string methods.
+Example:
+    Input: "Hello World"
+
+    Output: 2
+
+7. Password Strength Checker: Build a password strength checker that assesses a user's password based on criteria such as length, special characters, and uppercase/lowercase letters.
+Example:
+    Input: "HelloWorld"
+
+    Output:
+    Password length is too short.
+    Password does not contain any special characters.
+    Password does not contain any uppercase letters.
+
+8. Email Validation: Implement an email validator that checks whether a given string is a valid email address without using regular expressions or string methods.
+Example:
+    Input: abc@gmail.com
+
+    Output: Valid email address
+
+    Input: abc@gmail
+
+    Output: Invalid email address
+
+9. Palindrome Checker: Develop a program that determines if a given word or phrase is a palindrome (reads the same forwards and backwards) without using string reversal functions.
+Example:
+    Input: "racecar"
+
+    Output: "racecar is a palindrome"
+
+    Input: "hello"
+
+    Output: "hello is not a palindrome"
+
+10. Sentence Capitalization: Create a function that capitalizes the first letter of each word in a sentence without using any string methods.
+
+11. URL Parser: Design a program that parses a URL into its components (scheme, host, path, query, etc.) without using any string methods.
+Example:
+    Input: https://www.google.com/search?q=youtube
+
+    Output:
+    Scheme: https
+    Host: www.google.com
+    Path: /search
+    Query: q=youtube
+
+12. Anagram Checker: Write a function that checks if two words are anagrams of each other (contain the same characters in different orders) without using string sorting or manipulation functions.
+Example:
+    Input: "listen", "silent"
+
+    Output: "listen and silent are anagrams"
+
+    Input: "hello", "goodbye"
+
+    Output: "hello and goodbye are not anagrams"
+
 */
