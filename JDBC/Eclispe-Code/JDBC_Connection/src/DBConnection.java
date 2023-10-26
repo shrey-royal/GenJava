6a2a27a7a1b852578b16e70ae6aea83a974af22c
Shrey K <srkkadia@gmail.com>
2023-10-26 20:53:46 +0530

    JDBC Connection in Eclipse

---
+++ JDBC/Eclispe-Code/JDBC_Connection/src/DBConnection.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/genjava", "root", "root");
+
+            if(conn != null) {
+                System.out.println("Connected to database!");
+            } else {
+                System.out.println("Failed to connect to database!");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+	
+//	public static void main(String[] args) {
+//		getConnection();
+//	}
+}
